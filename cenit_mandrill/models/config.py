3b63b08d06a17611dfad689024188c62500fabf7
Maria Esther Guirola <marygh3008@gmail.com>
2016-06-02 12:31:30 -0400

    Changed the call to the collection's installer.

--- cenit_mandrill/models/config.py
+++ cenit_mandrill/models/config.py
@@ -100,11 +100,6 @@ class CenitIntegrationSettings(models.TransientModel):
                 id_: value
             })
 
-        installer.install_collection(
-            cr, uid,
-            data.get('id'),
-            params = params,
-            context = context
-        )
+        installer.pull_shared_collection(cr, uid, data.get('id'), context=context)
 
         return rc
