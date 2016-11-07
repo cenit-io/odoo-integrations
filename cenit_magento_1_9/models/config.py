034592b2b4b8aef5582557fc8f58fbceb5327b3a
Maria Esther Guirola <marygh3008@gmail.com>
2016-11-07 13:07:17 -0500

    Added connection and connection role post install.

--- cenit_magento_1_9/models/config.py
+++ cenit_magento_1_9/models/config.py
@@ -65,14 +65,55 @@ class CenitIntegrationSettings(models.TransientModel):
             context = context
         )
 
-        # params = {}
-        # for p in data.get('params'):
-        #     k = p.get('parameter')
-        #     id_ = p.get('id')
-        #     value = getattr(obj,COLLECTION_PARAMS.get(k))
-        #     params.update ({id_: value})
-
-        #installer.pull_shared_collection(cr, uid, data.get('id'), params=None, context=context)
         installer.install_collection(cr, uid, {'name': COLLECTION_NAME})
 
+        self.update_connection_role(cr, uid, context)
+        self.update_connection(cr, uid, context)
+
         #return rc
+
+    def update_connection(self, cr, uid, context):
+
+        conn_pool = self.pool.get("cenit.connection")
+        conn_id = conn_pool.search(cr, uid, [('name', '=', 'My Odoo host')])
+        conn = conn_pool.browse(cr, uid, conn_id[0], context) if conn_id else None
+        if conn:
+            conn_data = {
+                "_reference": "True",
+                "_primary": ["namespace", "name"],
+                "namespace": conn.namespace.id,
+                "name": "My Odoo host",
+                "headers": [{
+                            "key": "X_USER_ACCESS_KEY",
+                            "value": conn.key
+                            },
+                            {
+                             "key": "X_USER_ACCESS_TOKEN",
+                             "value": conn.token
+                            },
+                            {
+                             "key": "TENANT_DB",
+                             "value": conn.url
+                            },
+                            {
+                             "key": "COOKIE",
+                             "value": "session_id={% eval Devise.friendly_token %}"
+                            }
+                            ]
+
+            }
+            response = conn_pool.post(cr, uid, "/setup/connection", conn_data)
+
+    def update_connection_role(self, cr, uid, context):
+        role_pool = self.pool.get("cenit.connection.role")
+        conn_rol = role_pool.get(cr, uid, "/setup/connection_role", {'name': 'My Odoo role'})
+        if conn_rol:
+            if len(conn_rol["connection_role"]) > 0:
+                conn_rol = conn_rol["connection_role"][0]
+                webhook = {
+                    "_reference": "True",
+                    "namespace": "Odoo",
+                    "name": "Send order"
+                }
+                conn_rol["webhooks"].append(webhook)
+                role_pool.post(cr, uid, "/setup/connection_role", conn_rol)
