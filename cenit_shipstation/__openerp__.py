3b12e256072f7669df7b0565f47f4a5219c23fb0
Alain Fernández Deroncere <alainfd82@gmail.com>
2017-11-13 17:19:35 -0500

    Updated the icons of some add-ons

--- cenit_shipstation/__openerp__.py
+++ cenit_shipstation/__openerp__.py
@@ -34,8 +34,6 @@
         'view/config.xml',
         'view/wizard.xml'
     ],
-    'images': [
-        'static/images/banner.png'
-    ],
+    'images': ['static/images/banner.jpg'],
     'installable': True
 }
