3f435f11313e2d216d03c75f9b267927c2f251d5
Cesar Lage <kaerdsar@gmail.com>
2018-09-27 14:35:44 -0400

    [REF] remove deprecated modules, will be restored in next versions

--- cenit_twilio/models/config.py
+++ cenit_twilio/models/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
+#    Odoo, Open Source Management Solution
 #    Copyright (C) 2004-2010, 2014 Tiny SPRL (<http://tiny.be>).
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -26,85 +26,40 @@ from openerp import models, fields, api
 
 _logger = logging.getLogger(__name__)
 
-COLLECTION_NAME = "twilio"
+COLLECTION_NAME = "twilio_api_2010_04_01"
 COLLECTION_VERSION = "1.0.0"
 COLLECTION_PARAMS = {
-    'Account SID':'account_sid',
-    'Auth Token':'auth_token',
+    # WITHOUT COLLECTION_PARAMS.
 }
 
 class CenitIntegrationSettings(models.TransientModel):
-    _name = "cenit.twilio.settings"
+    _name = "cenit.twilio_api_2010_04_01.settings"
     _inherit = 'res.config.settings'
 
     ############################################################################
     # Pull Parameters
     ############################################################################
-    account_sid = fields.Char('Account SID')
-    auth_token = fields.Char('Auth Token')
+    # WITHOUT PULL PARAMETERS.
 
     ############################################################################
     # Default Getters
     ############################################################################
-    def get_default_account_sid(self, context):
-        account_sid = self.env['ir.config_parameter'].get_param(
-            'odoo_cenit.twilio.account_sid', default=None
-        )
-        return {'account_sid': account_sid or ''}
-
-    def get_default_auth_token(self, context):
-        auth_token = self.env['ir.config_parameter'].get_param(
-            'odoo_cenit.twilio.auth_token', default=None
-        )
-        return {'auth_token': auth_token or ''}
-
+    # WITHOUT GETTERS.
 
     ############################################################################
     # Default Setters
     ############################################################################
-    def set_account_sid(self):
-        config_parameters = self.env['ir.config_parameter']
-        for record in self.browse(self.ids):
-            config_parameters.set_param (
-                'odoo_cenit.twilio.account_sid', record.account_sid or ''
-            )
-
-    def set_auth_token(self):
-        config_parameters = self.env['ir.config_parameter']
-        for record in self.browse(self.ids):
-            config_parameters.set_param (
-                'odoo_cenit.twilio.auth_token', record.auth_token or ''
-            )
-
+    # WITHOUT SETTERS.
 
     ############################################################################
     # Actions
     ############################################################################
-    def execute(self):
-        rc = super(CenitIntegrationSettings, self).execute()
-
-        if not self.env.context.get('install', False):
-            return rc
-
-        objs = self.browse(self.ids)
-        if not objs:
-            return rc
-        obj = objs[0]
-
+    @api.model
+    def install(self):
         installer = self.env['cenit.collection.installer']
         data = installer.get_collection_data(
             COLLECTION_NAME,
             version = COLLECTION_VERSION
         )
 
-        params = {}
-        for p in data.get('pull_parameters'):
-            k = p['label']
-            id_ = p.get('id')
-            value = getattr(obj,COLLECTION_PARAMS.get(k))
-            params.update ({id_: value})
-
-        installer.pull_shared_collection(data.get('id'), params=params)
         installer.install_common_data(data['data'])
-
-        return rc
